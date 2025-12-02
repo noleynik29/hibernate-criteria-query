41a1af7bccd9817e9e98d3bb2d06cb7f78cfd150
noleynik29 <noleynik29@gmail.com>
2025-12-02 13:31:53 +0200

    Done

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -45,28 +45,16 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
         CriteriaBuilder cb = session.getCriteriaBuilder();
         CriteriaQuery<Phone> cq = cb.createQuery(Phone.class);
         Root<Phone> root = cq.from(Phone.class);
-        List<Predicate> predicates = new ArrayList<>();
-        if (params.containsKey("countryManufactured")) {
-            String[] countries = params.get("countryManufactured");
-            if (countries != null && countries.length > 0) {
-                predicates.add(root.get("countryManufactured").in((Object[]) countries));
-            }
-        }
-        if (params.containsKey("maker")) {
-            String[] makers = params.get("maker");
-            if (makers != null && makers.length > 0) {
-                predicates.add(root.get("maker").in((Object[]) makers));
-            }
-        }
-        if (params.containsKey("color")) {
-            String[] colors = params.get("color");
-            if (colors != null && colors.length > 0) {
-                predicates.add(root.get("color").in((Object[]) colors));
-            }
-        }
 
-        cq.where(predicates.toArray(new Predicate[0]));
+        List<Predicate> predicates = new ArrayList<>();
+
+        params.forEach((field, values) -> {
+            if (values != null && values.length > 0) {
+                predicates.add(root.get(field).in((Object[]) values));
+            }
+        });
+
+        cq.where(cb.and(predicates.toArray(new Predicate[0])));
         return session.createQuery(cq).getResultList();
     }
-
 }
